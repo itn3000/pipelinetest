e747c3235ccad60ee98e8e4bd1c29b617e590476
itn3000 <itn3000@gmail.com>
2017-02-09 16:39:24 +0900

    add rx and process example

--- Program.cs
+++ Program.cs
@@ -2,87 +2,233 @@ using System;
 
 namespace ConsoleApp
 {
+    using System.Reactive;
+    using System.Reactive.Linq;
     using System.IO;
     using System.IO.Pipelines;
     using System.Linq;
     using System.Collections.Generic;
-    static class EnumerableExtension
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+    using System.Text;
+    using System.Threading;
+    static class PipelineExtension
     {
-        public static IPipeReader CreateFromEnumerable(this PipeFactory pfac, IEnumerable<int> data)
+        public static IPipeReader CreateFromCommand(this PipeFactory pfac, string filePath, string args
+            , IPipeWriter stderr = null, IPipeReader stdin = null, CancellationToken ct = default(CancellationToken))
+        {
+            var si = new ProcessStartInfo(filePath, args);
+            si.RedirectStandardOutput = true;
+            si.RedirectStandardError = stderr != null;
+            si.RedirectStandardInput = stdin != null;
+            si.UseShellExecute = false;
+            si.CreateNoWindow = true;
+            return pfac.CreateReader(null, async (r, w) =>
+            {
+                try
+                {
+                    using (var proc = new Process())
+                    {
+                        proc.StartInfo = si;
+                        proc.Start();
+                        var procReader = proc.StandardOutput.BaseStream.AsPipelineReader();
+                        var errReader = stderr != null ? proc.StandardError.BaseStream.AsPipelineReader() : null;
+                        await Task.WhenAll(
+                            Task.Run(async () =>
+                            {
+                                if (stdin != null)
+                                {
+                                    await stdin.CopyToAsync(proc.StandardInput.BaseStream).ConfigureAwait(false);
+                                    proc.StandardInput.Dispose();
+                                }
+                            })
+                            ,
+                            Task.Run(async () =>
+                            {
+                                if (stderr != null)
+                                {
+                                    await errReader.CopyToAsync(stderr).ConfigureAwait(false);
+                                }
+                            })
+                            ,
+                            Task.Run(() =>
+                            {
+                                try
+                                {
+                                    using (ct.Register(() => proc.Kill()))
+                                    {
+                                        proc.WaitForExit();
+                                    }
+                                }
+                                catch (Exception e)
+                                {
+                                    procReader.Complete(e);
+                                    procReader.CancelPendingRead();
+                                    if (errReader != null)
+                                    {
+                                        errReader.CancelPendingRead();
+                                    }
+                                }
+                            })
+                            ,
+                            Task.Run(async () =>
+                            {
+                                await procReader.CopyToAsync(w).ConfigureAwait(false);
+                            })
+                        ).ConfigureAwait(false);
+                        w.Complete();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"{e}");
+                    w.Complete(e);
+                }
+            });
+        }
+        /// <summary>create pipeline reader from IEnumerable<T></summary>
+        public static IPipeReader CreateFromEnumerable<T>(this PipeFactory pfac, IEnumerable<T> data) where T : struct
         {
             return pfac.CreateReader(null, async (r, w) =>
             {
-                var tmp = new int[1];
+                var tmp = new T[1];
                 foreach (var b in data)
                 {
+                    // allocating write buffer
                     var wbuf = w.Alloc();
                     try
                     {
                         tmp[0] = b;
-                        var rspan = new Span<int>(tmp).AsBytes();
+                        var rspan = new Span<T>(tmp).AsBytes();
                         wbuf.Ensure(rspan.Length);
                         var span = wbuf.Memory.Span.Slice(0, rspan.Length);
                         rspan.CopyTo(span);
                         wbuf.Advance(rspan.Length);
-                        await wbuf.FlushAsync();
                     }
                     catch (Exception e)
                     {
                         Console.WriteLine($"error in reader:{e}");
-                        throw;
+                        await wbuf.FlushAsync();
+                        // inform exception to next reader
+                        w.Complete(e);
+                        // when exception is thrown to outside of function, pipe will be broken
+                        return;
                     }
+                    // must call FlushAsync after Advance
+                    await wbuf.FlushAsync();
                 }
+                // if you forget to call Complete(), following pipeline reader may get stack
                 w.Complete();
             });
         }
-    }
-    class Program
-    {
-        static IPipeWriter Filter0xf0(PipeFactory pfac, IPipeWriter writer)
+        // filtering test function for writer
+        public static IPipeWriter Filter0xf0(this PipeFactory pfac, IPipeWriter writer)
         {
             return pfac.CreateWriter(writer, async (r, w) =>
             {
                 while (true)
                 {
+                    var wbuf = w.Alloc();
                     try
                     {
-                        var wbuf = w.Alloc();
                         var result = await r.ReadAsync();
                         if (result.IsCancelled)
                         {
+                            // read cancelled
                             break;
                         }
                         if (result.IsCompleted && result.Buffer.IsEmpty)
                         {
+                            // read end
                             break;
                         }
+                        // ReadableBuffer is linked list of System.Memory<T>
                         foreach (var rbuf in result.Buffer)
                         {
+                            // allocate buffer for writing
                             wbuf.Ensure(rbuf.Length);
-                            var span = wbuf.Memory.Span;
-                            rbuf.CopyTo(span.Slice(0, rbuf.Length));
+                            // extract write buffer you want to use
+                            var span = wbuf.Memory.Span.Slice(0, rbuf.Length);
+                            // copy data to write buffer
+                            // OutOfRangeException when wbuffer is shorter than read buffer
+                            rbuf.CopyTo(span);
+                            // change value of write buffer
                             for (int i = 0; i < span.Length; i++)
                             {
                                 span[i] = (byte)(span[i] & 0x0f);
                             }
+                            // send write buffer to next writer
                             wbuf.Advance(rbuf.Length);
-                            await wbuf.FlushAsync();
                         }
+                        // consume read buffer
+                        // must call before next ReadAsync
                         r.Advance(result.Buffer.End);
                     }
                     catch (Exception e)
                     {
                         Console.WriteLine($"{e}");
                     }
+                    // must call FlushAsync before next Alloc
+                    await wbuf.FlushAsync();
+                }
+                w.Complete();
+            });
+        }
+        public static IObservable<ReadOnlyMemory<byte>> CreateObservable(this PipeFactory pfac, IPipeReader reader)
+        {
+            return Observable.Create<ReadOnlyMemory<byte>>(async (ob, ct) =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        ct.ThrowIfCancellationRequested();
+                        var result = await reader.ReadAsync();
+                        if (result.IsCancelled || (result.IsCompleted && result.Buffer.IsEmpty))
+                        {
+                            break;
+                        }
+                        foreach (var rbuf in result.Buffer)
+                        {
+                            ob.OnNext(rbuf);
+                        }
+                        reader.Advance(result.Buffer.End);
+                    }
+                    ob.OnCompleted();
+                }
+                catch (Exception e)
+                {
+                    try
+                    {
+                        reader.CancelPendingRead();
+                    }
+                    catch
+                    {
+
+                    }
+                    ob.OnError(e);
                 }
             });
         }
-        static void Main(string[] args)
+    }
+    class Program
+    {
+        static async Task ProcessPipeTest()
         {
             using (var pfac = new PipeFactory())
-            using (var ep = new MemoryStream())
+            using (var mstm = new MemoryStream())
             {
-                var rw = pfac.Create();
+                var reader = pfac.CreateFromCommand(@"c:\Windows\System32\cmd.exe", "/c \"dir c:\\\"");
+                await reader.CopyToAsync(mstm).ConfigureAwait(false);
+                Console.WriteLine($"{Encoding.GetEncoding(932).GetString(mstm.ToArray())}");
+            }
+        }
+        static void MemoryPipeTest()
+        {
+            using (var pfac = new PipeFactory())
+            {
+                // pipeline endpoint
+                // if you want only to output Stream, call AsPipelineWriter(this Stream) extension method or CopyToAsync(Stream)
                 var epwriter = pfac.CreateWriter(null, async (r, w) =>
                 {
                     while (true)
@@ -117,28 +263,35 @@ namespace ConsoleApp
                         }
                     }
                 });
-                var writer = Filter0xf0(pfac, epwriter);
-                var reader = pfac.CreateFromEnumerable(Enumerable.Range(0, 1000));
+                var writer = pfac.Filter0xf0(epwriter);
+                var reader = pfac.CreateFromEnumerable(Enumerable.Range(0, 10000));
                 reader.CopyToAsync(writer).Wait();
-                // Console.WriteLine($"ep length = {ep.Length}");
-                // var epar = ep.ToArray();
-                // for (int i = 0; i < epar.Length; i++)
-                // {
-                //     Console.Write($"{epar[i].ToString("x2")}");
-                //     if ((i & 0x1F) == 0x0F)
-                //     {
-                //         Console.WriteLine();
-                //     }
-                //     else
-                //     {
-                //         Console.Write(":");
-                //     }
-                // }
-                // if ((epar.Length & 0x1f) != 0x10)
-                // {
-                //     Console.WriteLine();
-                // }
             }
+        }
+        static void ObservableTest()
+        {
+            using(var pfac = new PipeFactory())
+            {
+                var r1 = pfac.CreateFromEnumerable(Enumerable.Range(0,10000));
+                pfac.CreateObservable(r1)
+                    .Subscribe(mem => {
+                        Console.WriteLine($"mem={mem}");
+                    },
+                    e => {
+                        Console.WriteLine($"onerror:{e}");
+                    },
+                    () => {
+                        Console.WriteLine($"oncompleted");
+                    });
+            }
+        }
+        static void Main(string[] args)
+        {
+            // for cp932 encoding
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            ProcessPipeTest().Wait();
+            MemoryPipeTest();
+            ObservableTest();
             Console.WriteLine("Hello World!");
         }
     }
